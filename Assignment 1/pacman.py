94df5c465a924a257646f705be598a6d99dcb43f
feforet <felix.foret@student.uclouvain.be>
2024-02-21 16:03:50 +0100

    ajout variable de position, code tests, reste a gerer les timeouts

--- Assignment 1/pacman.py
+++ Assignment 1/pacman.py
@@ -23,40 +23,34 @@ class Pacman(Problem):
     def actions(self, state):
         # Define the possible actions for a given state
         actions = []
-        for i in range(state.shape[0]):
-            for j in range(state.shape[1]):
-                if state.grid[i][j] == "P":
-                    pos = (i, j)
+        x, y = state.pos
 
-        x, y = pos
         for k in range(1, state.shape[0]):
             if x+k >= state.shape[0] or state.grid[x+k][y] == "#":
                 break
             actions.append(self.Action("S", k))
+
         for k in range(1, state.shape[0]):
             if x-k < 0 or state.grid[x-k][y] == "#":
                 break
             actions.append(self.Action("N", k))
+
         for k in range(1, state.shape[1]):
             if y+k >= state.shape[1] or state.grid[x][y+k] == "#":
                 break
             actions.append(self.Action("E", k))
+
         for k in range(1, state.shape[1]):
             if y-k < 0 or state.grid[x][y-k] == "#":
                 break
             actions.append(self.Action("W", k))
 
-        return actions
+        return tuple(actions)
 
 
     def result(self, state, action):
         # Apply the action to the state and return the new state
-        for i in range(state.shape[0]):
-            for j in range(state.shape[1]):
-                if state.grid[i][j] == "P":
-                    pos = (i, j)
-
-        x, y = pos
+        x, y = state.pos
         new_grid = [list(row) for row in state.grid]
         new_answer = state.answer
         new_grid[x][y] = "."
@@ -72,10 +66,10 @@ class Pacman(Problem):
         
         if new_grid[new_pos[0]][new_pos[1]] == "F":
             new_answer -= 1
+
         new_grid[new_pos[0]][new_pos[1]] = "P"
         
-        new_state = State(state.shape, tuple([tuple(row) for row in new_grid]), new_answer, "Move " + action.direction + " " + str(action.number_of_moves))
-        return new_state
+        return State(state.shape, tuple([tuple(row) for row in new_grid]), new_answer, "Move " + action.direction + " " + str(action.number_of_moves), new_pos)
         
     def goal_test(self, state):
     	#check for goal state
@@ -88,11 +82,20 @@ class Pacman(Problem):
 ###############
 class State:
 
-    def __init__(self, shape, grid, answer=None, move="Init"):
+    def __init__(self, shape, grid, answer=None, move="Init", pos=None):
         self.shape = shape
         self.answer = answer
         self.grid = grid
         self.move = move
+        self.pos=pos
+        if self.pos is None:
+            for i in range(self.shape[0]):
+                for j in range(self.shape[1]):
+                    if self.grid[i][j] == "P":
+                        self.pos = (i, j)
+                        break
+                if self.pos is not None:
+                    break
 
     def __str__(self):
         s = self.move + "\n"
@@ -115,24 +118,41 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Usage: ./Pacman.py <path_to_instance_file>")
     filepath = sys.argv[1]
+    if filepath == "test":
+        for i in range(1, 11):
+            path = f"Instances/i{i:02}"
+            shape, initial_grid, initial_fruit_count = read_instance_file(path)
+            init_state = State(shape, tuple(initial_grid), initial_fruit_count, "Init")
+            problem = Pacman(init_state)
 
-    shape, initial_grid, initial_fruit_count = read_instance_file(filepath) #On initialise les variables grâce à la lecture du file
-    init_state = State(shape, tuple(initial_grid), initial_fruit_count, "Init") #on met en tuple pour que ce soit immuable, on crée le initial state
-    problem = Pacman(init_state) #On définit le problème par la classe Pacman
+            for search_type in [breadth_first_tree_search, breadth_first_graph_search]:#, depth_first_tree_search, depth_first_graph_search]:
+                # pour les depth_search, on a un problème de stack overflow, il faut trouver comment limiter le temps d'execution de la fonction a 60sec
+                start_timer = time.perf_counter()
+                node, explored_nodes, remaining_nodes_queue = search_type(problem)
+                end_timer = time.perf_counter()
 
-    # Example of search
-    start_timer = time.perf_counter()
-    node, nb_explored, remaining_nodes = breadth_first_tree_search(problem)
-    end_timer = time.perf_counter()
+                timer = end_timer - start_timer
+                print(f"{path[10:]}\t| {timer:.10f}\t| {explored_nodes:10}\t| {remaining_nodes_queue:10}")
 
-    # Example of print
-    path = node.path()
 
-    for n in path:
-        # assuming that the __str__ function of state outputs the correct format
-        print(n.state)
+    else:
+        shape, initial_grid, initial_fruit_count = read_instance_file(filepath) #On initialise les variables grâce à la lecture du file
+        init_state = State(shape, tuple(initial_grid), initial_fruit_count, "Init") #on met en tuple pour que ce soit immuable, on crée le initial state
+        problem = Pacman(init_state) #On définit le problème par la classe Pacman
 
-    print("* Execution time:\t", str(end_timer - start_timer))
-    print("* Path cost to goal:\t", node.depth, "moves")
-    print("* #Nodes explored:\t", nb_explored)
-    print("* Queue size at goal:\t",  remaining_nodes)
+        # Example of search
+        start_timer = time.perf_counter()
+        node, nb_explored, remaining_nodes = breadth_first_graph_search(problem)
+        end_timer = time.perf_counter()
+
+        # Example of print
+        path = node.path()
+
+        for n in path:
+            # assuming that the __str__ function of state outputs the correct format
+            print(n.state)
+
+        print("* Execution time:\t", str(end_timer - start_timer))
+        print("* Path cost to goal:\t", node.depth, "moves")
+        print("* #Nodes explored:\t", nb_explored)
+        print("* Queue size at goal:\t",  remaining_nodes)
